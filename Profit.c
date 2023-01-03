ddb80ddca811dc86bdbbcef66dc236ba38632655
yuktany2 <119840650+yuktany2@users.noreply.github.com>
2023-01-03 10:42:59 +0530

    #include<stdio.h> int main () {     int cp,sp,amount;     scanf("%d%d",&cp,&sp);     if(sp>cp)     {         amount=sp-cp;         printf("Profit");     }     else if(cp>sp)     {         printf("Loss");     }     else     {         printf("No Profit No Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int cp,sp,amount;
+    scanf("%d%d",&cp,&sp);
+    if(sp>cp)
+    {
+        amount=sp-cp;
+        printf("Profit");
+    }
+    else if(cp>sp)
+    {
+        printf("Loss");
+    }
+    else
+    {
+        printf("No Profit No Loss");
+    }
+}
