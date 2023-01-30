48060e88b3cfa38a0ce72e68c3d703eb2cdc3dde
yuktany2 <119840650+yuktany2@users.noreply.github.com>
2023-01-30 09:44:51 +0530

    #include<stdio.h> int main () {     int n,reversed=0,remainder,original;     scanf("%d",&n);     original=n;     while(n!=0)     {         remainder=n%10;         reversed=reversed*10+remainder;         n/=10;
        }
        if(original==reversed)
        printf("True");
        else
        printf("False");
    }

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
 int main ()
 {
-   int n,q,r,s=0;
-   scanf("%d",&n);
-   q=n;
-   while(q!=0)
-   {
-       r=q%10;
-       s=s*10+r;
-       q=q/10;
-   }
-   if(n==s)
-   {
-       printf("Palindrome");
-   }
-   else
-   {
-       printf("Not Palindrome");
-   }
+    int n,reversed=0,remainder,original;
+    scanf("%d",&n);
+    original=n;
+    while(n!=0)
+    {
+        remainder=n%10;
+        reversed=reversed*10+remainder;
+        n/=10;
+        
+    }
+    if(original==reversed)
+    printf("True");
+    else
+    printf("False");
 }
